cf68ae940af03d8d285832a1dae4e633d776a1b1
Aditya Singh Rajawat <adityarajawat@gkmit.co>
2024-10-30 15:18:22 +0530

    feat: add connection with postgres and orm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { UserModule } from './modules/user/users.module';
+import { typeOrmConfig } from '../orm-config';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: './.env' });
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
 })
 export class AppModule {}
